a116e186efa299cf7d6d78f0996bab20795cff4f
Chamika Edirisinghe <edirisinghe.chamika.madushan@gmail.com>
2023-08-15 23:12:16 +0530

    first commit

---
+++ src/main/java/com/cmk/userauth/UserAuthProjectApplication.java
@@ -0,0 +1,13 @@
+package com.cmk.userauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserAuthProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserAuthProjectApplication.class, args);
+	}
+
+}
