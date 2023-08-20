2439589d9e70b28c76053e66647b9be87596db9d
Chamika Edirisinghe <edirisinghe.chamika.madushan@gmail.com>
2023-08-20 11:53:35 +0530

    Completed jwt token authorization

--- src/main/java/com/cmk/userauth/controller/AuthController.java
+++ src/main/java/com/cmk/userauth/controller/AuthController.java
@@ -2,13 +2,21 @@ package com.cmk.userauth.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cmk.userauth.entity.User;
+import com.cmk.userauth.payloads.JwtResponse;
+import com.cmk.userauth.payloads.LoginRequest;
+import com.cmk.userauth.payloads.MessageResponses;
 import com.cmk.userauth.repository.UserRepository;
+import com.cmk.userauth.security.jwt.JwtUtils;
 
 @RestController
 public class AuthController {
@@ -18,12 +26,18 @@ public class AuthController {
 
     @Autowired
     PasswordEncoder passwordEncoder;
+
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    @Autowired
+    JwtUtils jwtUtils;
     
     @PostMapping("/auth/register")
     public ResponseEntity<?> registerUser(@RequestBody User user){
         
         if(userRepository.existsByUsername(user.getUsername())){
-            return ResponseEntity.badRequest().body("Error: Username is already taken!");
+            return ResponseEntity.badRequest().body(new MessageResponses("Error: Username is already taken!"));
         }
 
         User newUser = new User();
@@ -31,8 +45,22 @@ public class AuthController {
         newUser.setPassword(passwordEncoder.encode(user.getPassword()));
 
         userRepository.save(newUser);
+
+        return ResponseEntity.ok(new MessageResponses("User registered successfully!"));
+    }
+
+    @PostMapping("/auth/login")
+    public ResponseEntity<?> loginUser(@RequestBody LoginRequest loginRequest){
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        String jwt = jwtUtils.generateJwtToken(authentication);
+
+        User user = userRepository.findByUsername(loginRequest.getUsername()).get();
         
-        return ResponseEntity.ok("User registered successfully!");
+        return ResponseEntity.ok(new JwtResponse(jwt, user.getId(), user.getUsername()));
     }
 
 }
---
+++ src/main/java/com/cmk/userauth/payloads/MessageResponses.java
@@ -0,0 +1,12 @@
+package com.cmk.userauth.payloads;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class MessageResponses {
+
+    private String message;
+    
+}
